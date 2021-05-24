2e74c2c4d03fb55264eeec4edf719703155bc1bf
Matthew Galloway <mgalloway@macbook-pro.home>
2021-05-24 09:22:03 +0100

    test structure

---
+++ tests/python/unit/test_binary_model.py
@@ -0,0 +1,5 @@
+from python-prediction-algo.glm-binary-classification import CustomPredictionAlgorithm
+
+def test_dummy_function():
+    dummy_results = dummy_function()
+    assert dummy_results == "foo"
