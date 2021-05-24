edc9e6c9cd940e0441fb7351632e58390ad44552
Matthew Galloway <52289894+matthewgalloway@users.noreply.github.com>
2021-05-24 10:47:50 +0100

    unit tests (#1)
    unit tests

---
+++ tests/python/unit/test_binary_model.py
@@ -0,0 +1,42 @@
+from python-prediction-algo.glm-binary-classification import CustomPredictionAlgorithm
+import statsmodel.api as sm
+import unittest
+
+def test_link_function():
+    case = unittest.TestCase()
+    expected = [
+        sm.families.links.cloglog(),
+        sm.families.links.log(),
+        sm.families.links.logit(),
+        sm.families.links.NegativeBinomial(1),
+        sm.families.links.Power(1),
+        sm.families.links.cauchy(),
+        sm.families.links.identity(),
+        sm.families.links.inverse_power(),
+        sm.families.links.inverse_squared()
+    ]
+
+    link_strings =[
+        'cloglog',
+        'log',
+        'logit',
+        'negative_binomial',
+        'power',
+        'cauchy',
+        'identity',
+        'inverse_power',
+        'inverse_squared'
+    ]
+
+    actual = []
+    for link in link_strings:
+        binary_model = CustomPredictionAlgorithm(
+            link=link,
+            family='binomial',
+            alpha=1,
+            power=1,
+            var_power=1
+        )
+        actual.append(binary_model.get_link())
+
+    case.assertItemsEqual(actual, expected)
---
+++ tests/python/unit/test_regression_model.py
@@ -0,0 +1,42 @@
+from python-prediction-algo.glm-regression import CustomPredictionAlgorithm
+import statsmodel.api as sm
+import unittest
+
+def test_link_function():
+    case = unittest.TestCase()
+    expected = [
+        sm.families.links.cloglog(),
+        sm.families.links.log(),
+        sm.families.links.logit(),
+        sm.families.links.NegativeBinomial(1),
+        sm.families.links.Power(1),
+        sm.families.links.cauchy(),
+        sm.families.links.identity(),
+        sm.families.links.inverse_power(),
+        sm.families.links.inverse_squared()
+    ]
+
+    link_strings =[
+        'cloglog',
+        'log',
+        'logit',
+        'negative_binomial',
+        'power',
+        'cauchy',
+        'identity',
+        'inverse_power',
+        'inverse_squared'
+    ]
+
+    actual = []
+    for link in link_strings:
+        regression_model = CustomPredictionAlgorithm(
+            link=link,
+            family='binomial',
+            alpha=1,
+            power=1,
+            var_power=1
+        )
+        actual.append(regression_model.get_link())
+
+    case.assertItemsEqual(actual, expected)
