3293832428ec0d064585573dd89204d7d9f69127
mgalloway <matthew.galloway19@gmail.com>
2021-07-13 09:00:57 +0100

    kwargs clean up

--- python-prediction-algos/generalized_linear_models-binary-classification/algo.py
+++ python-prediction-algos/generalized_linear_models-binary-classification/algo.py
@@ -6,19 +6,7 @@ class CustomPredictionAlgorithm(BaseCustomPredictionAlgorithm):
 
     def __init__(self, prediction_type=None, params=None):
         self.params = params
-        self.clf = BinaryClassificationGLM(
-            family_name=params.get("family"),
-            binomial_link=params.get("binomial_link"),
-            gamma_link=params.get("gamma_link"),
-            gaussian_link=params.get("gaussian_link"),
-            inverse_gaussian_link=params.get("inverse_gaussian_link"),
-            poisson_link=params.get("poisson_link"),
-            negative_binomial_link=params.get("negative_binomial_link"),
-            tweedie_link=params.get("tweedie_link"),
-            alpha=params.get("alpha"),
-            power=params.get("power"),
-            var_power=params.get("var_power")
-        )
+        self.clf = BinaryClassificationGLM(**params)
         super(CustomPredictionAlgorithm, self).__init__(prediction_type, self.params)
 
     def get_clf(self):
