9866cf9403f26289f47d734fe948d72ed5e260e3
mgalloway <matthew.galloway19@gmail.com>
2021-07-19 18:02:24 +0100

    Updating unit test with new structure

--- tests/python/unit/test_binary_model.py
+++ tests/python/unit/test_binary_model.py
@@ -1,37 +1,44 @@
+import sys
+sys.path.append("..")
+print('\n'.join(sys.path))
 from generalized_linear_models.dku_glm import BinaryClassificationGLM
 import statsmodels.api as sm
 import unittest
-from testing_utils import testing_dict
+from testing_utils import testing_dict_binary
+import numpy as np
+import pandas as pd
+from sklearn import datasets
 
 def test_link_function():
     case = unittest.TestCase()
     expected = [
-        sm.families.links.log(),
-        sm.families.links.identity(),
-        sm.families.links.inverse_power(),
-        sm.families.links.inverse_squared(),
-        sm.families.links.log(),
-        sm.families.links.cloglog(),
-        sm.families.links.Power(1),
+        type(sm.families.Binomial()),
+        type(sm.families.links.log())
     ]
 
     actual = []
-    for test in testing_dict:
+
+    breast_cancer = datasets.load_breast_cancer()
+    X, y = breast_cancer.data, breast_cancer.target
+
+    for test in testing_dict_binary:
+        test_params = testing_dict_binary[test]
         binary_model = BinaryClassificationGLM(
-            family=test['family'],
-            binomial_link=test['binomial_link'],
-            gamma_link=test['gamma_link'],
-            gaussian_link=['gaussian_link'],
-            inverse_gaussian_link=['inverse_gaussian_link'],
-            poisson_link=['poisson_link'],
-            negative_binomial_link=['negative_binomial_link'],
-            tweedie_link=['tweedie_link'],
+            family_name=test_params['family_name'],
+            binomial_link=test_params['binomial_link'],
+            gamma_link=test_params['gamma_link'],
+            gaussian_link=test_params['gaussian_link'],
+            inverse_gaussian_link=test_params['inverse_gaussian_link'],
+            poisson_link=test_params['poisson_link'],
+            negative_binomial_link=test_params['negative_binomial_link'],
+            tweedie_link=test_params['tweedie_link'],
             alpha=1,
             power=1,
             var_power=1
         )
-        binary_model.set_link_str()
-        actual.append(binary_model.get_link())
+        binary_model.fit_model(X, y)
+        actual.append(type(binary_model.family))
+        actual.append(type(binary_model.family.link))
 
-    case.assertItemsEqual(actual, expected)
+    case.assertListEqual(actual, expected)
 
--- tests/python/unit/test_regression_model.py
+++ tests/python/unit/test_regression_model.py
@@ -1,37 +1,44 @@
+import sys
+sys.path.append("..")
+print('\n'.join(sys.path))
 from generalized_linear_models.dku_glm import RegressionGLM
 import statsmodels.api as sm
 import unittest
-from testing_utils import testing_dict
+from testing_utils import testing_dict_regression
+import numpy as np
+import pandas as pd
+from sklearn import datasets
 
 def test_link_function():
     case = unittest.TestCase()
     expected = [
-        sm.families.links.log(),
-        sm.families.links.identity(),
-        sm.families.links.inverse_power(),
-        sm.families.links.inverse_squared(),
-        sm.families.links.log(),
-        sm.families.links.cloglog(),
-        sm.families.links.Power(1),
+        type(sm.families.Gaussian(sm.families.links.inverse_power())),
+        type(sm.families.links.inverse_power())
     ]
 
     actual = []
-    for test in testing_dict:
+
+    boston = datasets.load_boston()
+    X, y = boston.data, boston.target
+
+    for test in testing_dict_regression:
+        test_params = testing_dict_regression[test]
         regression_model = RegressionGLM(
-            family=test['family'],
-            binomial_link=test['binomial_link'],
-            gamma_link=test['gamma_link'],
-            gaussian_link=['gaussian_link'],
-            inverse_gaussian_link=['inverse_gaussian_link'],
-            poisson_link=['poisson_link'],
-            negative_binomial_link=['negative_binomial_link'],
-            tweedie_link=['tweedie_link'],
+            family_name=test_params['family_name'],
+            binomial_link=test_params['binomial_link'],
+            gamma_link=test_params['gamma_link'],
+            gaussian_link=test_params['gaussian_link'],
+            inverse_gaussian_link=test_params['inverse_gaussian_link'],
+            poisson_link=test_params['poisson_link'],
+            negative_binomial_link=test_params['negative_binomial_link'],
+            tweedie_link=test_params['tweedie_link'],
             alpha=1,
             power=1,
             var_power=1
         )
-        regression_model.set_link_str()
-        actual.append(regression_model.get_link())
+        regression_model.fit(X, y)
+        actual.append(type(regression_model.family))
+        actual.append(type(regression_model.family.link))
 
-    case.assertItemsEqual(actual, expected)
+    case.assertListEqual(actual, expected)
 
