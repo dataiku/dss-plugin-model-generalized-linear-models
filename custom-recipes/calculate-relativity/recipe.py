810729d9b9ea9ebe92fdd87d845718090a434dfa
Matthew Galloway <matthew.galloway@dataiku.com@uk-mac-mgallowa.home>
2024-01-19 19:29:08 +0000

    clean up

--- custom-recipes/calculate-relativity/recipe.py
+++ custom-recipes/calculate-relativity/recipe.py
@@ -1,34 +1,43 @@
 
 import dataiku
 import pandas as pd
+import logging
 
-from dataiku.customrecipe import get_input_names_for_role
-from dataiku.customrecipe import get_output_names_for_role
-from dataiku.customrecipe import get_recipe_config
+from dataiku.customrecipe import get_input_names_for_role, get_output_names_for_role, get_recipe_config
 from relativity_calculations.relativity_calculator import RelativityCalculator
 from relativity_calculations.base_calculations import calculate_base_values
 from dku_config.dku_model_handler import ModelHandler
-import logging
 from dataiku.customrecipe import get_recipe_config
+from dku_config import DkuConfig
 
 logger = logging.getLogger(__name__)
 
-recipe_config = get_recipe_config()
+dku_config = DkuConfig()
 
-logger.info(f"Recipe Config is{recipe_config}")
-logger.info(f"Inpouts are {get_input_names_for_role('input_ML_Model')}")
+dku_config.add_param(
+    name="dku_model",
+    value=get_input_names_for_role('input_ML_Model')[0],
+    required=True
+)
 
-dku_model = get_input_names_for_role('input_ML_Model')[0]
-
+dku_config.add_param(
+    name="relativities_output_dataset_name",
+    value=get_output_names_for_role('relativities_output')[0],
+    required=True
+)
 # Example usage
-model_handler = ModelHandler(dku_model)
+model_handler = ModelHandler(dku_config.dku_model)
 coefficients = model_handler.get_coefficients()
+
 preprocessed_df = model_handler.preprocess_dataframe(model_handler.get_dataframe('train'))
 base_values = calculate_base_values(preprocessed_df)
+
+
 relativity_calculator = RelativityCalculator(coefficients, base_values)
 relativities = relativity_calculator.calculate_relativities()
 
 variable_names = list(coefficients.keys())
+
 # Merge dictionaries
 combined_data = {var: {'Coefficient': coefficients[var], 
                        'Base Value': base_values[var], 
@@ -38,7 +47,6 @@ combined_data = {var: {'Coefficient': coefficients[var],
 df = pd.DataFrame.from_dict(combined_data, orient='index')
 
 
-output_dataset_name = get_output_names_for_role('output_dataset')[0]
 # Write recipe outputs
-nothing = dataiku.Dataset(output_dataset_name)
-nothing.write_with_schema(df)
+dku_output_dataset = dataiku.Dataset(dku_config.relativities_output_dataset_name)
+dku_output_dataset.write_with_schema(df.reset_index())
