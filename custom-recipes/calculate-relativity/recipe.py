420bf3b4e7fdf1fdd269d6650f18e3d3c8ab4f8d
Matthew Galloway <matthew.galloway@dataiku.com@uk-mac-mgallowa.home>
2024-01-19 12:46:10 +0000

    init commit-working progress

--- custom-recipes/calculate-relativity/recipe.py
+++ custom-recipes/calculate-relativity/recipe.py
@@ -1,72 +1,44 @@
-# Code for custom code recipe calculate-relativity (imported from a Python recipe)
 
-# To finish creating your custom recipe from your original PySpark recipe, you need to:
-#  - Declare the input and output roles in recipe.json
-#  - Replace the dataset names by roles access in your code
-#  - Declare, if any, the params of your custom recipe in recipe.json
-#  - Replace the hardcoded params values by acccess to the configuration map
-
-# See sample code below for how to do that.
-# The code of your original recipe is included afterwards for convenience.
-# Please also see the "recipe.json" file for more information.
-
-# import the classes for accessing DSS objects from the recipe
 import dataiku
-# Import the helpers for custom recipes
+import pandas as pd
+
 from dataiku.customrecipe import get_input_names_for_role
 from dataiku.customrecipe import get_output_names_for_role
 from dataiku.customrecipe import get_recipe_config
+from relativity_calculations.relativity_calculator import RelativityCalculator
+from relativity_calculations.base_calculations import calculate_base_values
+from dku_config.dku_model_handler import ModelHandler
+import logging
+from dataiku.customrecipe import get_recipe_config
 
-# Inputs and outputs are defined by roles. In the recipe's I/O tab, the user can associate one
-# or more dataset to each input and output role.
-# Roles need to be defined in recipe.json, in the inputRoles and outputRoles fields.
+logger = logging.getLogger(__name__)
 
-# To  retrieve the datasets of an input role named 'input_A' as an array of dataset names:
-input_A_names = get_input_names_for_role('input_A_role')
-# The dataset objects themselves can then be created like this:
-input_A_datasets = [dataiku.Dataset(name) for name in input_A_names]
+recipe_config = get_recipe_config()
 
-# For outputs, the process is the same:
-output_A_names = get_output_names_for_role('main_output')
-output_A_datasets = [dataiku.Dataset(name) for name in output_A_names]
+logger.info(f"Recipe Config is{recipe_config}")
+logger.info(f"Inpouts are {get_input_names_for_role('input_ML_Model')}")
 
+dku_model = get_input_names_for_role('input_ML_Model')[0]
 
-# The configuration consists of the parameters set up by the user in the recipe Settings tab.
+# Example usage
+model_handler = ModelHandler(dku_model)
+coefficients = model_handler.get_coefficients()
+preprocessed_df = model_handler.preprocess_dataframe(model_handler.get_dataframe('train'))
+base_values = calculate_base_values(preprocessed_df)
+relativity_calculator = RelativityCalculator(coefficients, base_values)
+relativities = relativity_calculator.calculate_relativities()
 
-# Parameters must be added to the recipe.json file so that DSS can prompt the user for values in
-# the Settings tab of the recipe. The field "params" holds a list of all the params for wich the
-# user will be prompted for values.
+variable_names = list(coefficients.keys())
+# Merge dictionaries
+combined_data = {var: {'Coefficient': coefficients[var], 
+                       'Base Value': base_values[var], 
+                       'Relativity': relativities[var]} for var in variable_names}
 
-# The configuration is simply a map of parameters, and retrieving the value of one of them is simply:
-my_variable = get_recipe_config()['parameter_name']
-
-# For optional parameters, you should provide a default value in case the parameter is not present:
-my_variable = get_recipe_config().get('parameter_name', None)
-
-# Note about typing:
-# The configuration of the recipe is passed through a JSON object
-# As such, INT parameters of the recipe are received in the get_recipe_config() dict as a Python float.
-# If you absolutely require a Python int, use int(get_recipe_config()["my_int_param"])
-
-
-#############################
-# Your original recipe
-#############################
-
-# -*- coding: utf-8 -*-
-import dataiku
-import pandas as pd, numpy as np
-from dataiku import pandasutils as pdu
+# Create DataFrame
+df = pd.DataFrame.from_dict(combined_data, orient='index')
 
 
-
-# Compute recipe outputs
-# TODO: Write here your actual code that computes the outputs
-# NB: DSS supports several kinds of APIs for reading and writing data. Please see doc.
-
-nothing_df = ... # Compute a Pandas dataframe to write into nothing
-
-
+output_dataset_name = get_output_names_for_role('output_dataset')[0]
 # Write recipe outputs
-nothing = dataiku.Dataset("nothing")
-nothing.write_with_schema(nothing_df)
+nothing = dataiku.Dataset(output_dataset_name)
+nothing.write_with_schema(df)
