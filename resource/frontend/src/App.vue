6101afe7c76055fda9ea5b813038424221b8c8c6
davidbehar <david.behar@dataiku.com>
2024-01-19 18:31:17 +0100

    loaded the BS framework but elements are not compiled correctly

--- resource/frontend/src/App.vue
+++ resource/frontend/src/App.vue
@@ -1,9 +1,81 @@
+<template>
+    <BsLayoutDefault ref="layout">
+        <BsTab
+            name="Variable Selection"
+            docTitle="Process Analyzer"
+            :docIcon="docLogo"
+        >
+            <BsTabIcon>
+                <img :src="firstTabIcon" alt="Target Definition Icon" />
+            </BsTabIcon>
+            <BsHeader>
+                <BsButton
+                    v-if="!layoutRef?.drawerOpen"
+                    flat
+                    round
+                    class="open-side-drawer-btn"
+                    size="15px"
+                    @click="closeSideDrawer"
+                    icon="mdi-arrow-right"
+                >
+                    <BsTooltip>Open sidebar</BsTooltip>
+                </BsButton>
+                <!-- <Header
+                    :target="store.draftTarget.target"
+                    :target-date="store.draftTarget.targetDate"
+                    :target-type="store.draftTarget.targetType"
+                ></Header> -->
+            </BsHeader>
+            <BsDrawer>
+              <div class="mb-3">
+                <label for="variableSelect" class="form-label">Choose a Variable</label>
+                <select class="form-select" id="variableSelect" v-model="selectedDefiningVariable">
+                  <option disabled value="">Please select a variable</option>
+                  <option v-for="variable in definingVariables" :key="variable" :value="variable">
+                    {{ variable }}
+                  </option>
+                </select>
+              </div>
+                <BsButton
+                    flat
+                    round
+                    class="close-side-drawer-btn"
+                    size="15px"
+                    @click="closeSideDrawer"
+                    icon="mdi-arrow-left"
+                >
+                    <BsTooltip>Close sidebar</BsTooltip>
+                </BsButton>
+            </BsDrawer>
+            <BsDocumentation>
+                <DocumentationContent></DocumentationContent>
+            </BsDocumentation>
+            <BsContent>
+                <div class="tab-content">
+                    <BarChart
+                        v-if="selectedDefiningVariable"
+                        :xaxisLabels="chartData.map(item => item.Category)"
+                        :barData="chartData.map(item => item.Value)"
+                        :observedAverageLine="chartData.map(item => item.observedAverage)"
+                        :fittedAverageLine="chartData.map(item => item.fittedAverage)"
+                        :chartTitle="selectedDefiningVariable"
+                      />
+                </div>
+            </BsContent>
+        </BsTab>
+        
+    </BsLayoutDefault>
+</template>
+
 <script lang="ts">
 import BarChart from './components/BarChart.vue'
 import * as echarts from "echarts";
 import type { DataPoint } from './models';
 import { defineComponent } from "vue";
 import { API } from './Api';
+import { BsLayoutDefault } from "quasar-ui-bs";
+import docLogo from "./assets/images/doc-logo-example.svg";
+import firstTabIcon from "./assets/images/first-tab-icon.svg";
 
 export default defineComponent({
     components: {
@@ -12,9 +84,11 @@ export default defineComponent({
     data() {
         return {
             chartData: [] as DataPoint[],
-            //definingVariables: "",
             selectedDefiningVariable: "",
             allData: [] as DataPoint[],
+            layoutRef: undefined as undefined | typeof BsLayoutDefault,
+            docLogo,
+            firstTabIcon,
         };
     },
     computed: {
@@ -30,6 +104,13 @@ export default defineComponent({
         this.chartData = this.allData.filter(item => item.definingVariable === newValue);
       }
     },
+    methods: {
+      closeSideDrawer() {
+            if(this.layoutRef){
+                this.layoutRef.drawerOpen = !this.layoutRef.drawerOpen;
+            }
+        },
+    },
     mounted() {
       API.getData().then((data: any) => {
         this.allData = data.data;
@@ -39,36 +120,6 @@ export default defineComponent({
 
 
 </script>
-
-<template>
-  <div class="container mt-3">
-    <!-- Title with Bootstrap display class and margin bottom utility -->
-    <h1 class="display-4 text-center mb-4">One Way Variable Analysis</h1>
-    
-    <!-- Dropdown with Bootstrap form-select class -->
-    <div class="mb-3">
-      <label for="variableSelect" class="form-label">Choose a Variable</label>
-      <select class="form-select" id="variableSelect" v-model="selectedDefiningVariable">
-        <option disabled value="">Please select a variable</option>
-        <option v-for="variable in definingVariables" :key="variable" :value="variable">
-          {{ variable }}
-        </option>
-      </select>
-    </div>
-    
-    <!-- Chart container -->
-    <div class="chart-container mb-3">
-      <BarChart
-        v-if="selectedDefiningVariable"
-        :xaxisLabels="chartData.map(item => item.Category)"
-        :barData="chartData.map(item => item.Value)"
-        :observedAverageLine="chartData.map(item => item.observedAverage)"
-        :fittedAverageLine="chartData.map(item => item.fittedAverage)"
-        :chartTitle="selectedDefiningVariable"
-      />
-    </div>
-  </div>
-</template>
 
 <style scoped>
 header {
