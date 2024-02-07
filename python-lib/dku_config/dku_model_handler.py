a6948b1f54fe1c3619d0644677379af7a6ea8ede
admin <admin>
2024-02-07 14:08:16 +0000

    Edited files

--- python-lib/dku_config/dku_model_handler.py
+++ python-lib/dku_config/dku_model_handler.py
@@ -120,7 +120,7 @@ class ModelHandler:
         for feature, values in self.relativities.items():
             for value, relativity in values.items():
                 coefficient = coefficients_dict.get(f"{feature}:{value}", None)
-                logger.info(f"Appending Coefficent {coefficient")
+                logger.info(f"Appending Coefficent {coefficient}")
                 
                 self.relativities_df = self.relativities_df.append({
                     'feature': feature,
