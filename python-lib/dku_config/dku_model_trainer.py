6d31b6355b05cc56cb3048e7b0d8ace987ffe9b5
admin <admin>
2024-03-21 08:33:18 +0000

    Edited files

--- python-lib/dku_config/dku_model_trainer.py
+++ python-lib/dku_config/dku_model_trainer.py
@@ -6,86 +6,85 @@ import logging
 class DataikuMLTask:
     """
     A class to manage machine learning tasks in Dataiku DSS.
+    
+    Attributes:
+        input_dataset (str): The name of the input dataset.
+        distribution_function (str): The distribution function used for the ML task.
+        link_function (str): The link function used in conjunction with the distribution function.
+        variables (list): A list of dictionaries, each representing a variable and its properties.
+        client: Instance of the Dataiku API client.
+        project: The default project from the Dataiku API client.
+        exposure_variable (str): Name of the exposure variable, if any.
+        weights_variable (str): Name of the weights variable, if any.
+        offset_variable (str): Name of the offset variable, if any.
     """
-
+    
     def __init__(self, input_dataset, distribution_function, link_function, variables):
         """
         Initializes the DataikuMLTask with the required parameters.
+        
+        Args:
+            input_dataset (str): The name of the input dataset.
+            distribution_function (str): The distribution function to use.
+            link_function (str): The link function to use.
+            variables (dict): A dictionary of variables with their properties.
         """
         self.client = dataiku.api_client()
-        logging.info("Dataiku API client initialized.")
-
         self.input_dataset = input_dataset
-        logging.info(f"Input dataset set to '{input_dataset}'.")
-
-        self.distribution_function = distribution_function.lower()
-        logging.info(f"Distribution function set to '{distribution_function.lower()}'.")
-
-        self.link_function = link_function.lower()
-        logging.info(f"Link function set to '{link_function.lower()}'.")
-
+        self.distribution_function = distribution_function
+        self.link_function = link_function
         self.variables = [{'name': key, **value} for key, value in variables.items()]
-        logging.info(f"Variables processed and set: {self.variables}")
-
         self.project = self.client.get_default_project()
-        logging.info("Default project obtained from Dataiku API client.")
-
+        
         # Initialize variables
         self.exposure_variable = None
         self.weights_variable = None
         self.offset_variable = None
-        self.target_variable = None  # Initialize target_variable
-
+        
         # Extract special variables based on their roles
         for variable in self.variables:
             role = variable.get("role")
-            if role == "Exposure":
+            if role == "exposure":
                 self.exposure_variable = variable['name']
-                logging.info(f"Exposure variable identified and set to '{self.exposure_variable}'.")
             elif role == "weights":
                 self.weights_variable = variable['name']
-                logging.info(f"Weights variable identified and set to '{self.weights_variable}'.")
             elif role == "offset":
                 self.offset_variable = variable['name']
-                logging.info(f"Offset variable identified and set to '{self.offset_variable}'.")
-            elif role == "Target":
-                self.target_variable = variable['name']
-                logging.info(f"Target variable identified and set to '{self.target_variable}'.")
 
+
+    
+    def disable_existing_variables(self):
+            # First, disable all existing variables
+        settings = self.mltask.get_settings()   
+        for feature_name in settings.get_raw()['preprocessing']['per_feature'].keys():
+            if feature_name != self.target_variable:
+                settings.reject_feature(feature_name)
+        settings.save()
+        
     def set_target(self):
         """
         Identifies and sets the target variable from the list of variables.
         Raises a ValueError if no target variable is found.
         """
-        found = False
         for variable in self.variables:
-            if variable.get("role") == "Target":
+            if variable.get("role") == "target":
                 self.target_variable = variable['name']
-                logging.info(f"Target variable set to '{self.target_variable}'.")
-                found = True
-                break
-        if not found:
-            message = "No target variable provided."
-            logging.error(message)
-            raise ValueError(message)
-    
-    def test_settings(self):
-        return self.mltask.get_settings()
-    
+                return
+        raise ValueError("No target variable provided")
+
+
     def update_mltask_modelling_params(self):
         """
         Updates the modeling parameters based on the distribution function, link function,
         and any special variables like exposure or offset.
         """
-        logging.info("Updating ML task modeling parameters.")
         settings = self.mltask.get_settings()
         algo_settings = settings.get_algorithm_settings('CustomPyPredAlgo_generalized-linear-models_generalized-linear-models_regression')
         algo_settings['params'].update({
             f"{self.distribution_function}_link": self.link_function,
             "family_name": self.distribution_function
         })
-        logging.info(f"Algorithm settings updated with distribution function '{self.distribution_function}' and link function '{self.link_function}'.")
-
+        
         # Handle exposure and offset variables
         if self.offset_variable and self.exposure_variable:
             algo_settings['params'].update({
@@ -94,21 +93,16 @@ class DataikuMLTask:
                 "exposure_columns": [self.exposure_variable],
                 "training_dataset": self.input_dataset
             })
-            logging.info(f"Exposure and offset variables set to '{self.exposure_variable}' and '{self.offset_variable}', respectively.")
         elif self.offset_variable:
             algo_settings['params'].update({
                 "offset_mode": "OFFSETS",
                 "offset_columns": [self.offset_variable],
                 "training_dataset": self.input_dataset
             })
-            logging.info(f"Offset variable set to '{self.offset_variable}'.")
         else:
             algo_settings['params']["offset_mode"] = "BASIC"
-            logging.info("No exposure or offset variables identified; default offset mode set to 'BASIC'.")
-
+        
         settings.save()
-        logging.info("ML task settings saved with updated modeling parameters.")
-
 
     
     def create_visual_ml_task(self):
@@ -116,7 +110,6 @@ class DataikuMLTask:
         Creates a new visual ML task in Dataiku.
         """
         self.set_target()
-        logging.info("Creating a new visual ML task in Dataiku.")
         # Create a new ML Task to predict the variable from the specified dataset
         self.mltask = self.project.create_prediction_ml_task(
             input_dataset=self.input_dataset,
@@ -124,92 +117,77 @@ class DataikuMLTask:
             ml_backend_type='PY_MEMORY',  # ML backend to use
             guess_policy='DEFAULT'  # Template to use for setting default parameters
         )
-        logging.info("New visual ML task created, waiting for guess to complete.")
         # Wait for the ML task to be ready
         self.mltask.wait_guess_complete()
-        logging.info("Guess completed for the ML task.")
-
+        
         self.update_mltask_modelling_params()
-        logging.info("ML task modelling parameters updated.")
-
+        
+        
         self.disable_existing_variables()
-        logging.info("Existing variables disabled.")
-
-    def disable_existing_variables(self):
-        """
-        First, disable all existing variables.
-        """
-        logging.info("Disabling all existing variables in ML task settings.")
-        settings = self.mltask.get_settings()   
-        for feature_name in settings.get_raw()['preprocessing']['per_feature'].keys():
-            if feature_name != self.target_variable:
-                settings.reject_feature(feature_name)
-                logging.info(f"Variable '{feature_name}' disabled.")
-        settings.save()
-        logging.info("ML task settings saved after disabling variables.")
 
     def enable_glm_algorithm(self):
         """
         Enables the GLM algorithm for the ML task.
         """
-        logging.info("Disabling all algorithms for the ML task.")
+        
         settings = self.mltask.get_settings()
         settings.disable_all_algorithms()
-        logging.info("All algorithms disabled.")
         
         # Assuming GLM is represented as 'GLM_REGRESSION' or 'GLM_CLASSIFICATION', adjust as necessary
         settings.set_algorithm_enabled("CustomPyPredAlgo_generalized-linear-models_generalized-linear-models_regression", True)
         settings.save()
-        logging.info("GLM algorithm enabled and settings saved.")
 
+    def test_settings(self):
+        return self.mltask.get_settings()
+    
     def configure_variables(self):
         """
         Configures the variables for the ML task, setting the type of processing for each.
         """
-        logging.info("Configuring variables for the ML task.")
         settings = self.mltask.get_settings()
         
         for variable in self.variables:
             variable_name = variable['name']
             if variable_name != self.target_variable and variable['role'] != 'reject':
                 settings.use_feature(variable_name)
-                logging.info(f"Variable '{variable_name}' used in ML task.")
                 fs = settings.get_feature_preprocessing(variable_name)
                 
                 # Configure categorical variables
                 if variable['type'] == 'categorical':
                     fs["category_handling"] = variable.get('processing', 'NONE')
-                    logging.info(f"Categorical variable '{variable_name}' processing set to '{variable.get('processing', 'NONE')}'.")
                 
                 # Configure numerical variables with specific processing types
                 elif variable['type'] == 'numerical':
                     processing = variable.get('processing', 'NONE')
                     if processing == 'standardize':
                         fs["rescaling"] = "STANDARD"
-                        logging.info(f"Numerical variable '{variable_name}' rescaling set to STANDARD.")
                     elif processing == 'normalize':
                         fs["rescaling"] = "MINMAX"
-                        logging.info(f"Numerical variable '{variable_name}' rescaling set to MINMAX.")
             elif variable_name == self.target_variable:
-                fs = settings.get_feature_preprocessing(variable_name)
-                fs["missing_handling"]= "IMPUTE"
+                    pass
             else:
                 settings.reject_feature(variable_name)
-                logging.info(f"Variable '{variable_name}' rejected and not used in ML task.")
-                
-        try:
-            logging.info(f"Attempting to save settings {settings.get_raw()}")
-            settings.save()
-        except:
-            logging.error(f"Failed to save settings. Error: {e}")
-            
-        logging.info("ML task settings updated with configured variables.")
+
+        settings.save()
+        print('***Updated settings***')
+        for feature in settings.get_raw().get('preprocessing').get('per_feature'):
+            print(f"Feature {feature} {settings.get_raw().get('preprocessing').get('per_feature').get(feature).get('role')} ")
+                            
+        return settings
+
+
+    def set_target_variable(self):
+        """
+        Sets the target variable for the ML task.
+        """
+        # This might be part of the task creation process, but if you need to adjust it:
+        settings = self.mltask.get_settings()
+        settings.set_target_variable(self.target_variable)
+        settings.save()
 
     def train_model(self):
         """
         Trains the model with the current configuration.
         """
-        logging.info(f"Starting model training with these settings {self.mltask.get_settings()}.")
         self.mltask.start_train()
         self.mltask.wait_train_complete()
-        logging.info("Model training completed.")
