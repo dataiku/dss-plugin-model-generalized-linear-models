4cf8095896f50440d2a77fd604839ec5e2a442e7
admin <admin>
2024-03-22 09:24:41 +0000

    Edited files

--- python-lib/dku_config/dku_model_trainer.py
+++ python-lib/dku_config/dku_model_trainer.py
@@ -173,7 +173,7 @@ class DataikuMLTask:
         fs['sendToInput'] = 'main'
         return fs
 
-    def update_to_categorical(self, fs, variable_preprocessing_method):
+    def update_to_categorical(self, fs, variable_preprocessing_method="CUSTOM"):
         
         fs['missing_impute_with']= 'MODE'
         fs['type']= 'CATEGORY'
@@ -203,6 +203,32 @@ class DataikuMLTask:
         fs['customHandlingCode'] = ''
         fs['customProcessorWantsMatrix'] = False
         fs['sendToInput'] = 'main'
+        fs['customHandlingCode'] = ('import numpy as np\n'
+        'import pandas as pd\n'
+        'class rebase_mode():\n'
+        '    """This processor applies dummy vectorisation, but drops the dummy column with the mode. Only applies to categorical variables\n'
+        '    """\n'
+        '    def __init__(self):\n'
+        '        self.mode_column = None\n'
+        '    def fit(self, series):\n'
+        '        # identify the mode of the column, returns as a text value\n'
+        '        self.modalities = np.unique(series)\n'
+        '        self.mode_column = series.mode()[0]\n'
+        '        self.columns = set(self.modalities)\n'
+        '        self.columns.remove(self.mode_column)\n'
+        '        self.column_name = series.name\n'
+        '    def transform(self, series):\n'
+        '        to_replace={m: self.mode_column for m in np.unique(series) if m not in self.modalities}\n'
+        '        new_series = series.replace(to_replace=to_replace)\n'
+        '        # obtains the dummy encoded dataframe, but drops the dummy column with the mode identified\n'
+        '        df = pd.get_dummies(new_series.values)\n'
+        '        if self.mode_column in df:\n'
+        '            df = df.drop(self.mode_column, axis = 1)\n'
+        '        for c in self.columns:\n'
+        '            if c not in df.columns:\n'
+        '                df[c] = 0\n'
+        '        return df\n'
+        'processor = rebase_mode()')
         
         return fs
     
@@ -220,7 +246,7 @@ class DataikuMLTask:
                 
                 # Configure categorical variables
                 if variable['type'] == 'categorical':
-                    variable_preprocessing_method = variable.get('processing', 'NONE')
+                    variable_preprocessing_method = variable.get('processing', None)
                     fs = self.update_to_categorical(fs, variable_preprocessing_method)
 
                 
