65c10b11df5b1fbe0bcd7bce5e916c64792b6e5f
admin <admin>
2024-04-10 08:59:23 +0100

    Edited files

--- python-lib/glm_handler/dku_model_handler.py
+++ python-lib/glm_handler/dku_model_handler.py
@@ -3,7 +3,7 @@ from dataiku.doctor.posttraining.model_information_handler import PredictionMode
 import pandas as pd
 import numpy as np
 from dataiku import pandasutils as pdu
-from dku_utils import extract_active_fullModelId
+from glm_handler.dku_utils import extract_active_fullModelId
 
 
 class ModelHandler:
