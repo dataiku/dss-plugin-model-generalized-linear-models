7531325435b0a64e25d46c98739d3c0e1935f2f5
admin <admin>
2024-04-10 09:42:40 +0100

    Edited files

--- python-lib/glm_handler/dku_model_deployer.py
+++ python-lib/glm_handler/dku_model_deployer.py
@@ -3,7 +3,7 @@ class ModelDeployer():
     
     def __init__(self, ml_task, saved_model_id):
         self.saved_model_id = saved_model_id
-        self.mltask = mltask
+        self.mltask = ml_task
         
     def set_new_active_version(self, model_id):
         logger.info(f"Model {model_id} deployed successfully.")
