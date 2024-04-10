8f3f7127046413e62e56cc65fbcc5b1ec8c5a349
admin <admin>
2024-04-10 09:05:15 +0100

    Edited files

--- python-lib/backend/fetch_api.py
+++ python-lib/backend/fetch_api.py
@@ -33,11 +33,11 @@ def get_models():
 
     try:
         # Assuming global_dku_mltask has a method to return trained model IDs
-        list_ml_id = global_dku_mltask.mltask.get_trained_models_ids()
+        list_ml_id = global_dku_mltask.get_trained_models_ids()
 
         models = []
         for ml_id in list_ml_id:
-            model_details = global_dku_mltask.mltask.get_trained_model_details(ml_id)
+            model_details = global_dku_mltask.get_trained_model_details(ml_id)
             model_name = model_details.get_user_meta()['name']
             models.append({"id": ml_id, "name": model_name})
 
