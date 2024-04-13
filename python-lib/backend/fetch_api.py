f818c5a7473c9205fdaac99fe974a42a5eb9800f
admin <admin>
2024-04-13 14:57:50 +0100

    Edited files

--- python-lib/backend/fetch_api.py
+++ python-lib/backend/fetch_api.py
@@ -190,29 +190,29 @@ def get_model_comparison_data():
     request_json = request.get_json()
     print(request_json)
     model1, model2 = request_json["model1"], request_json["model2"]
-    model1 ="A-SOL_CLAIM_MODELING_1-dba5M0pX-36k7GraX-s1-pp1-m1"
-    model2="A-SOL_CLAIM_MODELING_1-dba5M0pX-36k7GraX-s4-pp1-m1"
+    model1 ="A-SOL_CLAIM_MODELING_1-yifJ5kIw-RhJJw3L0-s10-pp1-m1"
+    model2="A-SOL_CLAIM_MODELING_1-yifJ5kIw-RhJJw3L0-s10-pp1-m1"
     
     model_deployer.set_new_active_version(model1)
     model_handler.update_active_version()
-    logger.info(f"Model {model1} is now the active version.")
-    model_1_lift_chart = model_handler.get_lift_chart(8)
+#     logger.info(f"Model {model1} is now the active version.")
+#     model_1_lift_chart = model_handler.get_lift_chart(8)
     
-    model_deployer.set_new_active_version(model2)
-    model_handler.update_active_version()
-    logger.info(f"Model {model2} is now the active version.")
+#     model_deployer.set_new_active_version(model2)
+#     model_handler.update_active_version()
+#     logger.info(f"Model {model2} is now the active version.")
 
-    model_2_lift_chart = model_handler.get_lift_chart(8)
+#     model_2_lift_chart = model_handler.get_lift_chart(8)
 
     
-    model_1_lift_chart.columns = ['Category', 'variable_values', 'observedAverage', 'Model_1_fittedAverage']
-    model_2_lift_chart.columns = ['Category', 'variable_values', 'observedAverage', 'Model_2_fittedAverage']
+#     model_1_lift_chart.columns = ['Category', 'variable_values', 'observedAverage', 'Model_1_fittedAverage']
+#     model_2_lift_chart.columns = ['Category', 'variable_values', 'observedAverage', 'Model_2_fittedAverage']
     
-    merged_model_stats = pd.merge(model_1_lift_chart, model_2_lift_chart, 
-                             on=['observedAverage','Category', 'variable_values'], 
-                             how='outer')
+#     merged_model_stats = pd.merge(model_1_lift_chart, model_2_lift_chart, 
+#                              on=['observedAverage','Category', 'variable_values'], 
+#                              how='outer')
     
-    merged_model_stats['exposure'] = 1
+#     merged_model_stats['exposure'] = 1
     
     return jsonify(merged_model_stats.to_dict('records'))
 # # local dev
