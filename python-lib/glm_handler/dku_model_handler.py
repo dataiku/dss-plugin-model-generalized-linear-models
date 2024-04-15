4020181803764be4846efa193966a9ecbe4990f6
admin <admin>
2024-04-15 17:12:20 +0200

    Edited file 'python-lib/glm_handler/dku_model_handler.py'

--- python-lib/glm_handler/dku_model_handler.py
+++ python-lib/glm_handler/dku_model_handler.py
@@ -323,7 +323,7 @@ class ModelHandler:
         coef_table = self.predictor._clf.coef_table.reset_index()
         coef_table[['dummy', 'variable', 'value']] = coef_table['index'].str.split(':', expand=True)
         coef_table['se_pct'] = coef_table['se']/abs(coef_table['coef'])*100
-        
+        print(coef_table)
         variable_stats = relativities.merge(coef_table[['variable', 'value', 'coef', 'se', 'se_pct']], how='left', left_on=['feature', 'value'], right_on=['variable', 'value'])
         variable_stats.drop('variable', axis=1, inplace=True)
         print(variable_stats)
