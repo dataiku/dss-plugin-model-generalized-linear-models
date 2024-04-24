689f287e8f2a569543922d7af781b5623f5531ac
admin <admin>
2024-04-24 10:53:57 +0100

    Edited files

--- python-lib/backend/local_config.py
+++ python-lib/backend/local_config.py
@@ -109,7 +109,7 @@ dummy_model_metrics ={
 def get_dummy_model_comparison_data():
     df = pd.DataFrame()
     df['Category'] = ["B0","B1","B2","B3","B4","B5","B6","B7","B8","B9","B10"]
-    df['exposure'] = np.random(30, 45, size=11)
+    df['exposure'] = np.random.uniform(30, 45, size=11)
     df['Model_1_fittedAverage'] =  np.random.uniform(50, 60, size=11)
     df['Model_2_fittedAverage'] =  np.random.uniform(45, 55, size=11)
     df['observedAverage'] =  np.random.uniform(15, 30, size=11)
