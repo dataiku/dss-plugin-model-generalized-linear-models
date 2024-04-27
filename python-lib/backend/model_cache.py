eec8d2c71be3141662f4fb682dceb2acb0628449
admin <admin>
2024-04-27 17:09:19 +0100

    Edited files

--- python-lib/backend/model_cache.py
+++ python-lib/backend/model_cache.py
@@ -32,21 +32,25 @@ def setup_model_cache(global_dku_mltask, model_deployer, model_handler):
     print(f"Model cache set up time took {model_cache_setup_time - time()}")
     return model_cache
 
-def update_model_cache(model_cache, model_handler):
+def update_model_cache(global_dku_mltask, model_cache, model_handler):
     
-    model_handler.update_active_version()
-    model1_predicted_base = model_handler.get_predicted_and_base()
-    model1_predicted_base.columns = ['definingVariable', 'Category', 'observedAverage', 'fittedAverage', 'Value', 'baseLevelPrediction']
-    mmc = ModelMetricsCalculator(model_handler)
-    model_1_aic, model_1_bic, model_1_deviance = mmc.calculate_metrics()
+    model_id_list = global_dku_mltask.get_trained_models_ids()
+    
+    for model_id in model_id_list:
+        if model_id not in model_cache.keys():
+            model_handler.update_active_version()
+            model1_predicted_base = model_handler.get_predicted_and_base()
+            model1_predicted_base.columns = ['definingVariable', 'Category', 'observedAverage', 'fittedAverage', 'Value', 'baseLevelPrediction']
+            mmc = ModelMetricsCalculator(model_handler)
+            model_1_aic, model_1_bic, model_1_deviance = mmc.calculate_metrics()
 
-    model_cache[model_id]= {
-        'predicted_and_base': model1_predicted_base,
-        'model_metrics': {
-            "AIC": model_1_aic,
-            "BIC": model_1_bic,
-            "Deviance": model_1_deviance
-        }
-    }
+            model_cache[model_id]= {
+                'predicted_and_base': model1_predicted_base,
+                'model_metrics': {
+                    "AIC": model_1_aic,
+                    "BIC": model_1_bic,
+                    "Deviance": model_1_deviance
+                }
+            }
         
     return model_cache
