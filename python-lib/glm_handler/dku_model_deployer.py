af7f311c53cc0b02e10845154fd1b1cf37d1b268
admin <admin>
2024-06-04 09:59:30 +0200

    Edited file 'python-lib/glm_handler/dku_model_deployer.py'

--- python-lib/glm_handler/dku_model_deployer.py
+++ python-lib/glm_handler/dku_model_deployer.py
@@ -5,9 +5,11 @@ logger = logging.getLogger(__name__)
 
 class ModelDeployer():
     
-    def __init__(self, ml_task, saved_model_id):
+    def __init__(self, saved_model, ml_task, saved_model_id):
+        self.saved_model = saved_model
         self.saved_model_id = saved_model_id
         self.mltask = ml_task
+        self.deployed_models = {self.saved_model.get_version_details(version['id']).details['smOrigin']['fullModelId']: version['id'] for version in self.saved_model.list_versions()}
         
     def set_new_active_version(self, model_id):
         logger.info(f"Model {model_id} deployed successfully.")
