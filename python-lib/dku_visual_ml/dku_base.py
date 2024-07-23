577c6ee5f3f4a6fc09ccaa85b72e8ebb968e5a6e
admin <admin>
2024-07-23 09:48:38 +0100

    creating dku base

---
+++ python-lib/dku_visual_ml/dku_base.py
@@ -0,0 +1,11 @@
+import dataiku
+from logging_assist.logging import logger
+class DataikuClientProject:
+    """
+    A base class to initialize Dataiku client and project
+    """
+    
+    def __init__(self):
+        self.client = dataiku.api_client()
+        self.project = self.client.get_default_project()
+        logger.info(f"Dataiku client and project initialized for project {self.project.project_key}")
